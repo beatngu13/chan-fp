05cbe112f019db36eeed0fbb7ea9dbf4f2cdd771
Daniel Kraus <daniel.kraus@mailbox.org>
2016-07-31 13:39:27 +0200

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject chan-fp "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Channel-based futures and promises for Clojure"
+  :url "https://github.com/beatngu13/chan-fp"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
