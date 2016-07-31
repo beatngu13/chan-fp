fa922bf960f5d6e70a8e7d3fea6fa7210e94f34e
Daniel Kraus <daniel.kraus@mailbox.org>
2016-07-31 13:34:29 +0200

    Initial

---
+++ src/chan_fp/core.clj
@@ -0,0 +1,57 @@
+(ns chan-fp.core
+  (:require [clojure.core.async
+             :refer [>! <!! alt!! chan go go-loop]]))
+
+;;; Future
+
+(defrecord Comp [value ok])
+
+(defn future [f]
+  (let [fut (chan)]
+    (go-loop [comp (f)]
+      (>! fut comp)
+      (recur comp))
+    fut))
+
+(defn get [fut]
+  (<!! fut))
+
+;;; Combinators
+
+(defn on-success [fut callback]
+  (go
+    (let [comp (get fut)]
+      (if (:ok comp)
+        (callback (:value comp))))))
+
+(defn on-failure [fut callback]
+  (go
+    (let [comp (get fut)]
+      (if (not (:ok comp))
+        (callback)))))
+
+(defn or-else [fut-a fut-b]
+  (future #(let [comp (get fut-a)]
+            (if (:ok comp)
+              comp
+              (get fut-b)))))
+
+(defn when [fut guard]
+  (future #(let [comp (get fut)
+                 value (:value comp)]
+            (Comp. value (and (:ok comp) (guard value))))))
+
+(defn then [fut f]
+  (future #(let [comp (get fut)]
+            (if (:ok comp)
+              (f (:value comp))
+              comp))))
+
+(defn any [fut-a fut-b]
+  (future #(letfn [(on-select [comp fut]
+                     (if (:ok comp)
+                       comp
+                       (get fut)))]
+            (alt!!
+              fut-a ([comp] (on-select comp fut-b))
+              fut-b ([comp] (on-select comp fut-a))))))
